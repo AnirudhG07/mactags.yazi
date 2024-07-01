6de9623f335e130d062e59981319d8bde78a1a10
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-07-01 22:00:56 +0530

    basic start

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # mactags.yazi
 Tagging files in yazi like MacOS.
+
+## Requirements
+- [yazi](https://github.com/sxyazi/yazi) Version >= 0.2.5
+- Tag, which you can install using `brew install tag`
+
+```bash
+tag - A tool for manipulating and querying file tags.
+  usage:
+    tag -a | --add <tags> <path>...     Add tags to file
+    tag -r | --remove <tags> <path>...  Remove tags from file
+    tag -s | --set <tags> <path>...     Set tags on file
+    tag -m | --match <tags> <path>...   Display files with matching tags
+    tag -f | --find <tags> <path>...    Find all files with tags (-A, -e, -R ignored)
+    tag -u | --usage <tags> <path>...   Display tags used, with usage counts
+    tag -l | --list <path>...           List the tags on file
+  <tags> is a comma-separated list of tag names; use * to match/find any tag.
+```
+
+## Tag names
+MacOS Finder has various tags based on priorities and usages. I have mapped each tag wit a single corresponding letter. The tags are as follows:
+```
+r = "red"
+b = "blue"
+g = "green"
+y = "yellow"
+o = "orange"
+p = "purple"
+a = "grey" -- because grey sounds like a
+h = "home"
+i = "important"
+w = "work"
+```
+
+## Usage
+
+### Keymaps
+Add the following line to your `keymap.toml` file-
+
+```toml
+on = ["b", "a"]
+run = "plugin mactags --args=add"
+desc = "Add multiple colored tags to a file/folder"
+```
+
+### Input
+I am hoping to have addition of tags as-
+```
+r g i w 
+```
+This will add all these 4 tags to the file/folder.
+
+## TODO's
+- [ ] - Write the add tags function for `tag -a`
+- [ ] - Write the remove tags function for `tag -r` (including remove all tags)
+- [ ] - Write the set tags function for `tag -s`
+- [ ] - Write the match tags function for `tag -m` h hopefully in fzf
+- [ ] - Write the find tags function for `tag -f` - hopefully in fzf
+- [ ] - Have Tag symbol(colored) displayed in the manager (without calling the plugin)
---
+++ init.lua
@@ -0,0 +1,159 @@
+local Tag_colors = { "r", "y", "o", "b", "g", " p", "a", "h", "i", "w" }
+
+local Color_mapping = {
+	r = "red",
+	b = "blue",
+	g = "green",
+	y = "yellow",
+	o = "orange",
+	p = "purple",
+	a = "grey", -- because grey sounds like a
+	h = "home",
+	i = "important",
+	w = "work",
+}
+local Shell_value = os.getenv("SHELL"):match(".*/(.*)")
+local LINUX_TEMP_PATH = "/.config/yazi/plugins/mactags.yazi/tags_storage"
+local WINDOWS_TEMP_PATH = "\\yazi\\config\\plugins\\tags-persistence.yazi\\tags_storage"
+
+local function colset_notify(str)
+	ya.notify({
+		title = "Mactags",
+		content = str,
+		timeout = 3,
+		level = "info",
+	})
+end
+
+local auto_generate_tag = function()
+	-- if input_tag is empty,auto find a col to bind from begin Tag_colors
+	local auto_assign_tag = "none" -- default assign tag is NONE
+	colset_notify("You have not assigned any tag.")
+
+	return auto_assign_tag
+end
+
+local assign_col = function(input_col)
+	-- input of this function is format {"g", "w", "i"}
+	-- Function to map input_col to single-character cols
+	local function map_cols(input_col)
+		local mapped_cols = {}
+		for _, color in ipairs(input_col) do
+			local lower_color = color:lower()
+			local col = Color_mapping[lower_color] or lower_color
+			if col then
+				table.insert(mapped_cols, col)
+			else
+				colset_notify("assign fail, invalid color: " .. color)
+				return nil
+			end
+		end
+		return mapped_cols
+	end
+	
+	local mapped_cols = map_cols(input_col)
+	-- Map the input_col to single-character cols
+	if not mapped_cols then
+		return nil
+	end
+
+	local unique_cols = {}
+	local unique_cols_set = {}
+
+	for _, col in ipairs(mapped_cols) do
+		if not unique_cols_set[col] then
+			table.insert(unique_cols, col)
+			unique_cols_set[col] = true
+		end
+	end
+
+    -- Convert the mapped cols to a space-separated string
+    local col_string = table.concat(mapped_cols, " ")
+
+    -- Check if the col_string is too long
+    local i = 0
+    for col in col_string:gmatch("%S+") do
+        i = i + 1
+        if i > 10 then -- since max 10 tags are possible
+            colset_notify("Assign fail, col too long. Assigning all the tags.")
+            return "red green blue yellow orange purple grey home important work"
+        end
+    end
+    return col_string
+end
+
+local function get_tags()
+	local col_set, event = ya.input({
+		realtime = false,
+		title = "set tag color(s) as - 'r g i w'",
+		position = { "top-center", y = 3, w = 40 },
+	})
+	if event == 1 and col_set ~= "" then
+		-- Split the input string into a table of cols
+        local cols = {}
+        for col in col_set:gmatch("%S+") do
+			table.insert(cols, col)
+		end
+		-- Checking input for errors
+		local assigned_cols = assign_col(cols)
+		-- assigned_cols is a string "red green blue"
+		if assigned_cols == nil then
+			return get_tags()
+		else
+			return event, assigned_cols
+		end
+
+	elseif event == 1 and col_set == "" then
+		local generate_col = auto_generate_tag()
+		return event, generate_col
+	else
+		return event, nil
+	end
+end
+
+local add_tags = function(event, generated_tags)
+    colset_notify("got flags as " .. generated_tags)
+	if generated_tags == "none" then
+		return
+	end
+
+	-- Split the input string into individual tags
+	local tags = {}
+	for tag in generated_tags:gmatch("%S+") do
+        tag = Color_mapping[tag] or tag
+		table.insert(tags, tag)
+	end
+
+	-- Run the command separately for each tag
+	for _, tag in ipairs(tags) do
+        colset_notify("Assigning tag: " .. tag)
+        -- TILL HERE IT WORKS. file path is yet to be given
+        local cmd_args = "tag -a " .. tag .. file_path -- not given yet
+        if event == 1 then
+            ya.manager_emit("shell", {
+                Shell_value .. " -c " .. ya.quote(cmd_args .. "; exit", true),
+                block = true,
+                confirm = true,
+            })
+        end
+	end
+
+	return true
+end
+
+return {
+	entry = function(_, args)
+		local action = args[1]
+		if not action then
+			return
+		end
+
+		if action == "add" then
+			local event, col = get_tags()
+			if col == nil then
+				return
+			end
+			add_tags(event, col)
+		end
+	end,
+}
